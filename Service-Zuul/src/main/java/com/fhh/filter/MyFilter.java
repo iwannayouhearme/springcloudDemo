d931dc8a513b1de1ec1ac5bb4925de33edc15dbe
fhh <351892572@qq.com>
2018-09-26 14:12:14 +0800

    增加zuul过滤器

---
+++ Service-Zuul/src/main/java/com/fhh/filter/MyFilter.java
@@ -0,0 +1,55 @@
+package com.fhh.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+/**
+ * 功能描述：（）
+ * Created by biubiubiu小浩 on 2018-09-26 13:16
+ */
+@Component
+public class MyFilter extends ZuulFilter {
+    private static Logger logger = LoggerFactory.getLogger(MyFilter.class);
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        logger.info(String.format("---------",request.getMethod(),request.getRequestURI().toString()));
+        String token = request.getParameter("token");
+        if (StringUtils.isEmpty(token)){
+            logger.warn("token is empty");
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseStatusCode(401);
+            try {
+                ctx.getResponse().getWriter().write("token is empty");
+            } catch (IOException e) {
+                return null;
+            }
+        }
+        logger.info("ok");
+        return null;
+    }
+}
